6f84a23beeaf5929e1cc534af3c529e9787f8dba
EmanAbobakr <43084491+EmanAbobakr@users.noreply.github.com>
2023-10-30 17:08:21 +0200

    add navigator

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { CategoriesScreen } from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <CategoriesScreen></CategoriesScreen>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={CategoriesScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
